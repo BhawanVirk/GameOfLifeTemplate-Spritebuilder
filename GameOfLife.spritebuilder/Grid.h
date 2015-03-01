30c8749c0c43e56d7a156bd4d4acc602f70c747d
Bhawan Virk <bhawanvirk@Bhawans-MacBook-Pro.local>
2015-03-01 12:03:52 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Grid.h
@@ -0,0 +1,13 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Bhawan Virk on 3/1/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@end
---
+++ GameOfLife.spritebuilder/Grid.m
@@ -0,0 +1,13 @@
+//
+//  Grid.m
+//  GameOfLife
+//
+//  Created by Bhawan Virk on 3/1/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Grid.h"
+
+@implementation Grid
+
+@end
